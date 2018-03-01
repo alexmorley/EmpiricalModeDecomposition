5af80297d485116123bc1de2ecf0bfdbadfc384d
Alexander Morley <alexander.morley@univ.ox.ac.uk>
2018-03-01 11:59:28 +0000

    make sure always at least one extrema

--- src/util.jl
+++ src/util.jl
@@ -5,5 +5,18 @@ function findExtrema(signal::AbstractArray)
     minima  = [x.I[1] for x in Images.findlocalminima(signal, 1)]
     minvals = signal[minima]
     
+    ## make sure at length extrema > 1        
+    if isempty(maxvals)
+        mm,tt = findmax(tempy)
+        push!(maxima,tt)
+        push!(maxvals,mm)
+    end
+    
+    if isempty(minvals)
+        mm,tt = findmin(tempy)
+        push!(minima,tt)
+        push!(minvals,mm)
+    end
+
     return maxima,minima,maxvals,minvals
 end
